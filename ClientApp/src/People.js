6004d3055832310b25e811500e6bd4dade49554a
Yocheved Rochel Wajsbort <cheviwaj@gmail.com>
2022-05-01 01:23:48 -0400

    Add project files.

---
+++ ClientApp/src/People.js
@@ -0,0 +1,53 @@
+﻿import React from 'react';
+import AddPersonForm from './AddPersonForm';
+import PeopleTable from './PeopleTable';
+
+class People extends React.Component {
+
+    state = {
+        people: [],
+        person: {}
+    }
+
+    onFirstNameChange = e => {
+        const { lastName, age } = this.state.person;
+        this.setState({ person: {firstName: e.target.value, lastName, age} });
+    }
+
+    onLastNameChange = e => {
+        const { firstName, age } = this.state.person;
+        this.setState({ person: { firstName, lastName: e.target.value, age } });
+    }
+
+    onAgeChange = e => {
+        const { firstName, lastName } = this.state.person;
+        this.setState({ person: { firstName, lastName, age: e.target.value } });
+    }
+
+    onAddClick = () => {
+        const { people, person } = this.state;
+        const copy = [...people, person];
+        this.setState({ people: copy, person: {firstName: '', lastName: '', age: ''} });
+        console.log(this.state.person);
+    }
+
+    onClearAllClick= () => {
+        this.setState({ people: [] });
+    }
+    render() {
+        return (
+            <div className='container mt-5'>
+                <AddPersonForm
+                    onFirstNameChange={this.onFirstNameChange}
+                    onLastNameChange={this.onLastNameChange}
+                    onAgeChange={this.onAgeChange}
+                    onAddClick={this.onAddClick}
+                    onClearAllClick={this.onClearAllClick}
+                    person={this.state.person}   />
+                <PeopleTable people={this.state.people}/>
+            </div>
+        )
+    }
+}
+
+export default People;
---
+++ ClientApp/src/PeopleTable.js
@@ -0,0 +1,49 @@
+﻿import React from 'react';
+
+class PeopleTable extends React.Component{
+
+    generateTable = (people) => {
+        if(people.length === 0){
+            return <h1>No people added yet! Add some people!</h1>
+        }  return (
+            <div>
+                <table className='table table-hover table-striped table bordered'>
+                    <thead>
+                        <tr>
+                            <th>First Name</th>
+                            <th>Last Name</th>
+                            <th>Age</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {people.map(this.generateRow)} 
+                    </tbody>
+                </table>
+            </div>
+
+        )
+    };
+
+    generateRow = ({firstName, lastName, age }, key) => {
+
+        let className = '';
+        if (age > 65 ){
+            className = 'table-danger';
+        }
+
+        return <tr key={key} className={className}>
+            <td>{firstName}</td>
+            <td>{lastName}</td>
+            <td>{age}</td>
+        </tr>;
+    }
+
+    
+    render(){
+        const people = this.props.people;
+        return this.generateTable(people);
+    }
+    
+}
+
+export default PeopleTable;
